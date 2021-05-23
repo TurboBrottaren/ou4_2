de4815e56de7bc3518b728300249db216667aac0
TurboBrottaren <84684246+TurboBrottaren@users.noreply.github.com>
2021-05-23 23:51:46 +0200

    nu så

--- heltal.cpp
+++ heltal.cpp
@@ -1,18 +1,43 @@
 #include <cstdlib>
 // Integer class 
 
+"""
+Solutions to module 4
+Student: Oskar Ekstrand
+Mail: oskar.ekstrand.1905@student.uu.se
+Reviewed by:
+Reviewed date: 
+"""
+
 class Heltal{
 	public:
 		Heltal(int);
 		int get();
+		int fib();
 		void set(int);
+			
 	private:
+		
 		int val;
+		int fibcpp(int);
 	};
  
 Heltal::Heltal(int n){
 	val = n;
 	}
+
+int Heltal::fibcpp(int n){
+    if (n <= 1)
+        return n;
+	else
+    	return fibcpp(n-1) + fibcpp(n-2);
+}
+
+int Heltal::fib(){
+	int n = val;
+	return fibcpp(n);
+
+}
  
 int Heltal::get(){
 	return val;
@@ -22,10 +47,10 @@ void Heltal::set(int n){
 	val = n;
 	}
 	
-
 extern "C"{
 	Heltal* Heltal_new(int n) {return new Heltal(n);}
 	int Heltal_get(Heltal* heltal) {return heltal->get();}
+	int Heltal_fib(Heltal* heltal) {return heltal->fib();}
 	void Heltal_set(Heltal* heltal, int n) {heltal->set(n);}
 	void Heltal_delete(Heltal* heltal){
 		if (heltal){
---
+++ test_ja.py
@@ -0,0 +1,2 @@
+n = [i for i in range(30,46)]
+print(n)
